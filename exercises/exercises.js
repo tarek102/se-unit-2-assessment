e8af2ffbaf09ea81c9d4c85fa07c99c57197102a
unknown <tarekyosry8@gmail.com>
2023-07-15 22:58:39 +0300

    solve 2nd exercise

--- exercises/exercises.js
+++ exercises/exercises.js
@@ -5,12 +5,23 @@ function stripPunctuation(e) {
 
 // Question 2
 function rotateArray() {
-
+    if(arr.length == 0){
+        return []
+    } else if (Array.isArray(arr)){
+        let rotatedArr = [];
+        for (let i=0; i < arr.length; i++){
+            rotatedArr.push(arr[i])
+        }
+        rotatedArr.push(rotatedArr.shift())
+        return rotatedArr
+    } else {
+        return undefined
+    }
 }
 
 // Question 3
 function letterCaseCounts() {
-
+    
 }
 
 // Don't write below this line...
